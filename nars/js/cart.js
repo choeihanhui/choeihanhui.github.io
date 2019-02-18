65df8bf0371c8941b3bd3dc55a773479b8dbcaa2
choeihanhu <choeihanhui@gmail.com>
2019-02-18 16:27:37 +0900

    nars

--- nars/js/cart.js
+++ nars/js/cart.js
@@ -37,12 +37,12 @@ $(function(){
         //위시리스트 뿌리기
         $('#tab2 ul').append(
           '<li data-key="'+key+'">'+
-            '<label><input type="checkbox" class="wselectall"><i class="icon icon-check"></i></label>'+
+            '<label><input type="checkbox" class="select-wish"><i class="icon icon-check"></i></label>'+
             '<div class="wish-image products"><a href="#"><img src="'+src+'" alt="shopping"></a></div>'+
             '<dl class="cart-infos">'+
               '<dt>'+name+'</dt>'+
               // '<dd>수량<div class="count"><button class="icon icon-minus" ></button><span class="num">1</span><button class="icon icon-plus"></button></div></dd>'+
-              '<dd>가격 : <span>'+price+'</span>원</dd>'+
+              '<dd>가격 : <span class="price">'+price+'</span>원</dd>'+
             '</dl>'+
             '<div class="btn-group">'+
               '<button class="icon icon-cart"></button>'+
@@ -56,12 +56,16 @@ $(function(){
         //장바구니 넣기
         $('#tab1 ul').append(
             '<li data-key="'+key+'">'+
-              '<label><input type="checkbox" class="sselectall"><i class="icon icon-check"></i></label>'+
+              '<label><input type="checkbox" class="select-cart"><i class="icon icon-check"></i></label>'+
               '<div class="wish-image products"><a href="#"><img src="'+src+'" alt="shopping"></a></div>'+
               '<dl class="cart-infos">'+
                 '<dt>'+name+'</dt>'+
-                '<dd>수량<div class="count"><button class="icon icon-minus" ></button><span class="num">1</span><button class="icon icon-plus"></button></div></dd>'+
-                '<dd>가격 : <span>'+price+'</span>원</dd>'+
+                '<dd>수량<div class="count">'+
+                  '<button class="icon icon-minus"><span class="blind">minus</span></button>'+
+                  '<span class="num">1</span>'+
+                  '<button class="icon icon-plus"><span class="blind">plus</span></button></div>'+
+                '</dd>'+
+                '<dd>가격 : <span class="price">'+price+'</span>원</dd>'+
               '</dl>'+
               '<div class="btn-group">'+
                 '<button class="icon icon-heart"></button>'+
@@ -87,80 +91,180 @@ $(function(){
               $(this).remove();
             }
           })
+          totalPrice();
         }
       }
     }
-    //카트 휴지통 아이콘 클릭 => 삭제
-    $('.cart .icon-transh').on('click',function(){
-      var popkey= $(this).parents('li').data('key');
-      // var slidekey = $('.swiper-slide').data('key');
-        $('.bestseller .swiper-slide').each(function(){
-          var slidekey = $(this).data('key');
-          console.log(slidekey);
-          if (popkey == slidekey) {
-            $(this).find('.icon-cart').removeClass('on');
-          }
-        })
-      $(this).parents('li').remove();
+  })
+
+  //카트 휴지통 아이콘 클릭 => 삭제
+  $('.cart').on('click', '.icon-transh',function(){
+    var popkey= $(this).parents('li').data('key');
+    // var slidekey = $('.swiper-slide').data('key');
+      $('.bestseller .swiper-slide').each(function(){
+        var slidekey = $(this).data('key');
+        // console.log(slidekey);
+        if (popkey == slidekey) {
+          $(this).find('.icon-cart').removeClass('on');
+        }
+      })
+    $(this).parents('li').remove();
+
+    var count=0;
+    $('#tab1 .select-cart').each(function(){
+      if($(this).prop('checked')){
+        count++;
+      }
     })
-    //카트 휴지통 아이콘 클릭 => 삭제
-    $('.wish .icon-transh').on('click',function(){
-      var popkey= $(this).parents('li').data('key');
-      // var slidekey = $('.swiper-slide').data('key');
-        $('.bestseller .swiper-slide').each(function(){
-          var slidekey = $(this).data('key');
-          console.log(slidekey);
-          if (popkey == slidekey) {
-            $(this).find('.icon-heart').removeClass('on');
-          }
-        })
-      $(this).parents('li').remove();
+
+    $('#tab1 .select-num').text(count);
+    totalPrice();
+  })
+
+  //카트 휴지통 아이콘 클릭 => 삭제
+  $('.wish').on('click','.icon-transh',function(){
+    var popkey= $(this).parents('li').data('key');
+    // var slidekey = $('.swiper-slide').data('key');
+      $('.bestseller .swiper-slide').each(function(){
+        var slidekey = $(this).data('key');
+
+        if (popkey == slidekey) {
+          $(this).find('.icon-heart').removeClass('on');
+        }
+      })
+    $(this).parents('li').remove();
+  })
+
+  $('.cart').on('click','.count button',function(){
+    var count = $(this).parent('.count').find('.num').text();
+    if($(this).text()=='plus'){
+      count++;
+      if (count > 10) {
+        count=10;
+        alert('최대수량입니다.')
+      }
+    }else{
+      count--;
+      if (count < 1) {
+        count=1;
+        alert('최소수량입니다.')
+      }
+    }
+
+    $(this).parent('.count').find('.num').text(count);
+
+    totalPrice();
+  })
+
+  function totalPrice(){
+    //체크된것만 총금액에 합산하기
+    var totalprice=0;
+    $('.cart li').each(function(){
+      if($(this).find('[type="checkbox"]').prop('checked')){
+        var num = $(this).find('.num').text();
+        var price = $(this).find('.price').text().replace(/[^0-9]/g,'');
+        totalprice+=price*num;
+        console.log(totalprice);
+      }
     })
-  })
-  var count=0;
-  $('.cart').on('click','.icon-plus',function(){
-    count = $(this).prev('.num').text();
-    count++;
-    if (count > 10) {
-      count=10;
-      alert('최대수량입니다.')
-    }
-    $(this).prev('.num').text(count)
-  })
-  $('.cart').on('click','.icon-minus',function(){
-    count = $(this).next('.num').text();
-    count--;
-    if (count < 1) {
-      count=1;
-      alert('최소수량입니다.')
-    }
-    $(this).next('.num').text(count)
-  })
+    $('.totalprice').text(numberWithCommas(totalprice));
+  }
+
+  //3자리씩 끊어서 쉼표붙이는 함수
+  function numberWithCommas(x) {
+      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
   //체크박스 쇼핑카트
   $('#cart-all').change(function(){
-    console.log(count);
     var count = $('.cart').find('li').length;
-    // var number = $('.cart').find('li').data('key');
     if ($(this).prop('checked')==true) {
-      $('.sselectall').attr('checked',true);
-      $('.select-num').text(count);
-      $('.cart-total a').click(function(){
-        $('.cart').find('li').remove();
-      })
+      $('.select-cart').prop('checked',true);
+      $('#tab1 .select-num').text(count);
     } else {
-      $('.sselectall').attr('checked',false);
+      $('.select-cart').prop('checked',false);
+      $('.select-num').text('0');
+      $('.cart').find('li').show();
+    }
+    totalPrice();
+  })
+
+  // 체크박스 위시리스트
+  $('#wish-all').change(function(){
+    var countW = $('.wish').find('li').length;
+    if ($(this).prop('checked')==true) {
+      $('.select-wish').prop('checked',true);
+      $('#tab2 .select-num').text(countW);
+    } else {
+      $('.select-wish').prop('checked',false);
       $('.select-num').text('0');
     }
   })
-  // 체크박스 위시리스트
-  $('#wish-all').click(function(){
-    if ($(this).prop('checked')==true) {
-      $('.wselectall').prop('checked',true);
-    } else {
-      $('.wselectall').prop('checked',false);
+
+  //리스트 체크박스
+  $('.tab-contents-body').on('change','[type="checkbox"]',function(){
+    var id=$(this).parents('.tab-content').attr('id');
+    var count=0;
+    if(id=='tab1'){
+      $('#cart-all').prop('checked',false);
+      $('#'+id).find('li').each(function(){
+        if($(this).find('[type="checkbox"]').prop('checked')){
+            count++;
+        }
+      })
+      $('#'+id).find('.select-num').text(count);
+    }else{
+      $('#wish-all').prop('checked',false);
+    }
+    totalPrice();
+  })
+
+  //선택상품삭제
+  $('.select-del').click(function(e){
+    e.preventDefault();
+    console.log('선택삭제누름');
+    var id=$(this).parents('.tab-content').attr('id');
+    $('.select-num').text(0);
+    if(id=='tab1'){//카트삭제
+      $('#cart-all').prop('checked',false);
+      $('.cart li').each(function(){
+        if($(this).find('[type="checkbox"]').prop('checked')){
+          var key=$(this).data('key');
+          $('.bestseller .swiper-slide').each(function(){
+            if(key==$(this).data('key')){
+              $(this).find('.icon-cart').removeClass('on');
+            }
+          })
+          $(this).remove();
+        }
+      })
+      totalPrice();
+      if($('.cart li').length==0){
+        $('.nonecart').show();
+      }
+    }else{//위시리스트삭제
+      console.log('위시삭제');
+      $('#wish-all').prop('checked',false);
+      $('.wish li').each(function(){
+        if($(this).find('[type="checkbox"]').prop('checked')){
+          var key=$(this).data('key');
+          $('.bestseller .swiper-slide').each(function(){
+            if(key==$(this).data('key')){
+              $(this).find('.icon-heart').removeClass('on');
+            }
+          })
+          $(this).remove();
+        }
+      })
+
+      if($('.wish li').length==0){
+        $('.nonewish').show();
+      }
     }
   })
-  // 수량 더하기 빼기
+
+
+
 
 
 })
